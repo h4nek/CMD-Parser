c7de3169b9810897d154f30bc22c48226eab2a5d
h4nek <42340131+h4nek@users.noreply.github.com>
2020-07-01 20:43:18 +0200

    predefined generic Enum type

---
+++ src/options/EnumOption.java
@@ -0,0 +1,31 @@
+package options;
+
+import java.util.List;
+
+/**
+ * An option accepting an {@code Enum} argument. Requires the concrete Enum type to correctly parse the parameter.
+ */
+public class EnumOption<E extends Enum<E>> extends Option<Enum<E>>{
+    Class<E> enumType;
+
+    public EnumOption(List<String> aliases, String description, boolean mandatory, Class<E> enumType) {
+        super(aliases, description, mandatory);
+        this.enumType = enumType;
+    }
+
+    public EnumOption(List<String> aliases, String description, boolean mandatory, Enum<E> defaultValue, Class<E> enumType) {
+        super(aliases, description, mandatory, defaultValue);
+        this.enumType = enumType;
+    }
+
+    @Override
+    void parse(String parameter) {
+//        this.getClass().getGenericSuperclass()
+//        for (Enum<E> constant : argument.getClass().getEnumConstants()) {
+//            if (constant.toString().equals(parameter)) {
+//                argument = constant;
+//            }
+//        }
+        Enum.valueOf(enumType, parameter);
+    }
+}
